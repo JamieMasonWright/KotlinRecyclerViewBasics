492b603f5bbadef5de2de80ac28cae9c8f54f65d
Jamie Mason_Wright <jamie.mason-wright@lush.co.uk>
2019-05-08 16:18:20 +0100

    Initial commit

---
+++ app/src/main/java/com/strategicchaos/kotlinrecyclerviewbasics/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.strategicchaos.kotlinrecyclerviewbasics
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        layoutManager = LinearLayoutManager(this)
+        recyclerview_view.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerview_view.adapter = adapter
+    }
+}
